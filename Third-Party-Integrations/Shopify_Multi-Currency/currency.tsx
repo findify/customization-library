177ce0a7c7470627617c4715168333ce9726ade0
Christin Mikolai <mail@christinmikolai.de>
2022-01-06 19:45:13 +0100

    add a list with currencies

---
+++ Third-Party-Integrations/Shopify_Multi-Currency/currency.tsx
@@ -0,0 +1,81 @@
+/* DONE:
+  - price product cards + works for autocomplete product cards + rec widgets
+  - range facet filter options + input fields, breadcrumbs automatically work with filter updates it seems
+*/
+
+/*
+  List of currencies
+    - BRL
+    - CAD
+    - COP
+    - EUR
+    - GBP
+    - MXN
+    - USD
+*/
+
+export default {
+  "BRL": {
+    "code": "BRL",
+    "symbol": "R$",
+    "thousandsSeparator": ",",
+    "decimalSeparator": ".",
+    "symbolOnLeft": true,
+    "spaceBetweenAmountAndSymbol": false,
+    "decimalDigits": 2
+  },
+  "CAD": {
+    "code": "CAD",
+    "symbol": "$",
+    "thousandsSeparator": ",",
+    "decimalSeparator": ".",
+    "symbolOnLeft": true,
+    "spaceBetweenAmountAndSymbol": false,
+    "decimalDigits": 2
+  },
+  "COP": {
+    "code": "COP",
+    "symbol": "$",
+    "thousandsSeparator": ",",
+    "decimalSeparator": ".",
+    "symbolOnLeft": true,
+    "spaceBetweenAmountAndSymbol": false,
+    "decimalDigits": 2
+  },
+  "EUR": {
+    "code": "EUR",
+    "symbol": "€",
+    "thousandsSeparator": " ",
+    "decimalSeparator": ",",
+    "symbolOnLeft": true,
+    "spaceBetweenAmountAndSymbol": true,
+    "decimalDigits": 2
+  },
+  "GBP": {
+    "code": "GBP",
+    "symbol": "£",
+    "thousandsSeparator": ",",
+    "decimalSeparator": ".",
+    "symbolOnLeft": true,
+    "spaceBetweenAmountAndSymbol": false,
+    "decimalDigits": 2
+  },
+  "MXN": {
+    "code": "MXN",
+    "symbol": "$",
+    "thousandsSeparator": ",",
+    "decimalSeparator": ".",
+    "symbolOnLeft": true,
+    "spaceBetweenAmountAndSymbol": false,
+    "decimalDigits": 2
+  },
+  "USD": {
+    "code": "USD",
+    "symbol": "$",
+    "thousandsSeparator": ",",
+    "decimalSeparator": ".",
+    "symbolOnLeft": true,
+    "spaceBetweenAmountAndSymbol": false,
+    "decimalDigits": 2
+  }
+}
