6eb74e12bdc083148a4e8aefd79ff76d2a28227f
Christin Mikolai <mail@christinmikolai.de>
2022-01-06 19:48:27 +0100

    add conversion for prices (grid, autocomplete, rec widget)

--- Third-Party-Integrations/Shopify_Multi-Currency/components-Cards-Product-Price-index.tsx
+++ Third-Party-Integrations/Shopify_Multi-Currency/components-Cards-Product-Price-index.tsx
@@ -8,6 +8,9 @@ import { IProduct, ThemedSFCProps } from 'types';
 import { priceIsSampleArray } from 'helpers/getPrice';
 import { useConfig } from '@findify/react-connect';
 import styles from 'components/Cards/Product/Price/styles.css';
+// import custom currencies and the helper function to convert prices
+import currency from 'currency';
+import convertPrice from 'convertPrice';
 
 /** List of props that Price component accepts */
 export interface IPriceProps extends ThemedSFCProps {
@@ -25,6 +28,17 @@ export default ({ className, theme = styles, item }: IPriceProps) => {
 
   const hasCompare = !!item.get('compare_at');
 
+  // define the variables we will need to convert below
+  const price = item.get('price');
+  const compare_at = item.get('compare_at');
+
+  // update both price and sale prices
+  const updated_price = price.size > 0 && price.setIn([0], convertPrice(price.get(0))).setIn([price.size-1],convertPrice(price.get(price.size-1)));
+  const updated_compare_at = compare_at != -1 && convertPrice(compare_at);
+
+  // grab Shopify currencies
+  const multiCurrency = currency[Shopify.currency.active] || config.get('currency');
+
   return (
     <div className={cx(theme.priceWrapper, className)}>
       <span
@@ -33,10 +47,12 @@ export default ({ className, theme = styles, item }: IPriceProps) => {
           (hasDiscount || hasCompare) && theme.salePrice
         )}
       >
-        {getPrice(item.get('price'), config.get('currency'))}
+        {/* pass the updated values */}
+        {getPrice(updated_price, multiCurrency)}
       </span>
       <span display-if={hasCompare} className={cx(theme.compare)}>
-        {getPrice(item.get('compare_at'), config.get('currency'))}
+        {/* pass the updated values */}
+        {getPrice(updated_compare_at, multiCurrency)}
       </span>
     </div>
   );
