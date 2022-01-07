e700cb64ab94722f9238bd3fc5c171e5dbae2dbe
Christin Mikolai <mail@christinmikolai.de>
2022-01-07 15:05:08 +0100

    formatting

--- Third-Party-Integrations/Shopify_Multi-Currency/convertPrice.tsx
+++ Third-Party-Integrations/Shopify_Multi-Currency/convertPrice.tsx
@@ -4,10 +4,10 @@ const convertPrice = (price) => {
   // default_currency is the default currency that is set for your store, string that has this format: "USD", "GBP", "EUR" and so on
   const default_currency = "USD";
 
-  if(Shopify.currency.active !== default_currency){
+  if (Shopify.currency.active !== default_currency) {
     const convertedPrice = (window.Shopify.currency.rate * price).toFixed(2);
 
-    if(Shopify.currency.active == 'EUR'){
+    if (Shopify.currency.active == 'EUR') {
       return Math.floor(convertedPrice) + 0.95;
     }
     return Math.ceil(convertedPrice);
